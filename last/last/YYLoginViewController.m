e7811d112ea29bbb2d70771f18a175f121c5b73a
谢雪彬 <q131968850@qq.com>
2016-05-30 17:21:28 +0800

    触摸手势以及抖动特效

--- last/last/YYLoginViewController.m
+++ last/last/YYLoginViewController.m
@@ -9,10 +9,17 @@
 #import "YYLoginViewController.h"
 #import "YYYanzhenViewController.h"
 #import "AppDelegate.h"
+#import "AFViewShaker.h"
+
 @interface YYLoginViewController ()
 @property (weak, nonatomic) IBOutlet UITextField *userID;
 @property (weak, nonatomic) IBOutlet UITextField *userPW;
 
+@property (nonatomic, strong) IBOutletCollection(UIView) NSArray * allTextFields;
+
+@property (strong, nonatomic) UITapGestureRecognizer *tapTrick;
+
+@property (nonatomic, strong) AFViewShaker * viewShaker;
 
 @end
 
@@ -20,6 +27,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.viewShaker = [[AFViewShaker alloc] initWithViewsArray:self.allTextFields];
     self.navigationController.navigationBarHidden = YES;
     UIImageView *image=[[UIImageView alloc] initWithImage:[UIImage imageNamed:@"图层-6"]];
     _userID.leftView=image;
@@ -32,11 +40,29 @@
     _userPW.rightViewMode = UITextFieldViewModeAlways;
     // Do any additional setup after loading the view.
 
+    //初始化手势并且申明手势触发的事件的方法名keyboardTrick
+    _tapTrick = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(keyboardTrick:)];
+    //将手势取消激活
+    _tapTrick.enabled = NO;
+    //将手势加入根视图
+    [self.view addGestureRecognizer:_tapTrick];
+    
+    //监听键盘打开这一操作，打开后执行keyboardWillShow:方法
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
+    //监听键盘收起这一操作，收起后执行keyboardWillHide:方法
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillHide:) name:UIKeyboardWillHideNotification object:nil];
+}
+
+- (UIStatusBarStyle)preferredStatusBarStyle {
+    return UIStatusBarStyleLightContent;
+}
+
+- (IBAction)onShakeOneAction:(UIButton *)sender {
+    [self.viewShaker shake];
 }
 
 -(void)viewWillAppear:(BOOL)animated{
     self.navigationController.navigationBarHidden = YES;
-
 
 }
 
@@ -45,6 +71,27 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(void)keyboardTrick:(UITapGestureRecognizer *)tap{
+    //判断触摸事件是否成功被识别
+    if (tap.state == UIGestureRecognizerStateRecognized) {
+        //NSLog(@"被摸了");
+        //[_textField resignFirstResponder];
+        //让根视图终止编辑状态，从而达到收起键盘的目的
+        [self.view endEditing:YES];
+    }
+}
+
+//键盘打开时的操作
+- (void)keyboardWillShow:(NSNotification *)notification{
+    //激活手势
+    _tapTrick.enabled = YES;
+}
+
+//键盘收起时的操作
+- (void)keyboardWillHide:(NSNotification *)notification{
+    //关闭手势
+    _tapTrick.enabled = NO;
+}
 /*
 #pragma mark - Navigation
 
